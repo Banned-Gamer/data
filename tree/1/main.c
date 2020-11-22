0e27c4e38b7bdd1363d46344bb17eae57f467a83
banned <banned@bupt.edu.cn>
2020-11-22 12:19:56 +0800

    beta version

---
+++ tree/1/main.c
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+struct tree{
+    char data;
+    tree *left,*right;
+};
+typedef tree* node;
+
+node head;
+
+void pre_tree(){
+    node list=new(node);
+    
+}
+int main(){
+    
+}

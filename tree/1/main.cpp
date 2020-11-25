da069ce1099a7269ab6a980498335a48734ebf1a
banned <banned@bupt.edu.cn>
2020-11-25 23:50:01 +0800

    beta 2.0

---
+++ tree/1/main.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+#define N 114514
+
+struct tree{
+    char data;
+    tree *left,*right;
+};
+typedef tree* node;
+
+node head;
+char a_tree[N];
+int x,long_tree;
+void pre_tree(){
+    node list;
+	list = new tree;
+    list->left = NULL;
+    list->right = NULL;
+    head = list;
+    return;
+}
+
+char node_tree(node now)
+{
+    if (a_tree[x] == '*') return;
+	
+    node next;
+
+	/*********先生成左子树**********/
+    next = new tree;
+    next->left = NULL;
+    next->right = NULL;
+    next->data = a_tree[x++];
+    now->left = next;
+    node_tree(next);
+
+	/*********后生成右子树**********/
+    next = new tree;
+    next->left = NULL;
+    next->right = NULL;
+    if (a_tree[x] != '*') next->data = a_tree[x++];
+    else return;
+    now->right = next;
+    node_tree(next);
+	
+	return;
+}
+
+void add_tree()
+{
+    cin >> a_tree;
+    long_tree = strlen(a_tree);
+    pre_tree();
+    node_tree(head);
+    return;
+}
+
+void print(node now)
+{
+    cout << now->data;
+
+    int flag1 = (now->left == NULL);
+    int flag2 = (now->right == NULL);
+    if (flag1 && flag2)
+    {
+        cout << ")";
+        return;
+    }
+    cout << ",(";
+    if (!flag1) print(now->left);
+    if ((!flag1) && (!flag2)) cout << ",";
+    if (!flag2) print(now->right);
+    return;
+}
+
+int main(){
+    add_tree();
+    print(head);
+    return;
+}
---
+++ tree/2/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	FILE* IN = fopen("in.txt", "rb");
+	FILE* OUT = fopen("out.txt", "wb");
+}
